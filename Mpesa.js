900bed514bbd8637703e695d19e039eb6cbab924
zablon-ke <zablonupwork@gmail.com>
2024-01-28 00:39:26 +0300

    added save mpesa data to db

--- Mpesa.js
+++ Mpesa.js
@@ -3,9 +3,22 @@ import axios from "axios";
 import jwt from 'jsonwebtoken';
 import fs from 'fs'
 import cors from 'cors'
+import bodyParser from 'body-parser';
+import { randomUUID } from 'crypto';
 const route=express.Router()
 
 route.use(cors())
+const verifyToken=(req,res,next)=>{
+    const secret=process.env.SECRET || crypto.randomBytes(32).toString("hex")
+   const token=req.headers.authorization.split(" ")[1]
+   jwt.verify(token,secret,(err,decoded)=>{
+    if(err){
+      return  res.status(401).json({"success":false,"message":"session expired"})
+    }
+    req.user=decoded
+    next()
+   })
+}
 const accessToken = (req, res, next)=> {
     const auth = {'Authorization': 'Basic ' +Buffer.from(process.env.CONSUMER_KEY+":"+process.env.CONSUMER_SECRET).toString('base64')};
     const url="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
@@ -60,6 +73,15 @@ const stkPush=(amount,phone,req,res)=>{
         let ResponseDescription= data['ResponseDescription']
         let CustomerMessage= data['CustomerMessage']
         console.log(CheckoutRequestID)
+        
+        let trans_ID=randomUUID().substring(randomUUID().length-10,randomUUID().length ).toUpperCase()
+        req.mysql.query("insert into transactions(trans_ID,checkout_ID,user_ID) values(?,?,?)",[trans_ID,CheckoutRequestID,req.user.user_id],(err,results)=>{
+            if(err){
+                console.log(err)
+                return
+            }
+            
+         })  
         fs.appendFile("checkouts.txt",CheckoutRequestID+" "+new Date().getFullYear()+""+new Date().getMonth()+1+""+new Date().getDate()+""+new Date().getHours()+" \n",(e=>{
             if(e){
                 console.log(e)
@@ -87,7 +109,8 @@ const checkTransaction=(req,res)=>{
         "Password": Buffer.from(`${600977}${process.env.PASSKEY}${timestamp}`).toString("base64"),
         "Timestamp": timestamp,  
         "CheckoutRequestID": checkouts[0],    
-     }    
+     }  
+    
      axios.post(url,payload,{headers})
      .then(response=>{
         console.log(response)
@@ -126,7 +149,7 @@ route.post("/validate",(req,res)=>{
     console.log(req.body)
 })
 
-route.post("/stk",accessToken,(req,res)=>{
+route.post("/stk",verifyToken,accessToken,(req,res)=>{
     
      let {phone}=req.body
      stkPush("1",phone,req,res)
